ab1ca97f89b9d748f07b88aa04a547ad8976a6f4
Geo54321 <tech@geoderp.com>
2024-06-26 13:28:43 -0500

    modified:   src/main/java/com/geoderp/geoplugin/Commands/GeoInfo.java

--- src/main/java/com/geoderp/geoplugin/Commands/GeoInfo.java
+++ src/main/java/com/geoderp/geoplugin/Commands/GeoInfo.java
@@ -9,7 +9,7 @@ public class GeoInfo implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage("§2For more info on the custom features you can view them on the MCNSA website at: https://mcnsa.com/custom-features");
-        sender.sendMessage("§2For evern more info follow the Github links on the page on the MCNSA website.");
+        sender.sendMessage("§2For even more info follow the Github links on the page on the MCNSA website.");
         return false;
     }
     
